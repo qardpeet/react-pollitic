c477cb4d2ffcae23f54b74e90ddac325da32ea03
qardpeet <patplaysg@gmail.com>
2018-11-05 12:32:43 +0400

    Added infinite scrolling pagination to PollsFull component

--- src/ChildComponents/PollsFull.js
+++ src/ChildComponents/PollsFull.js
@@ -11,25 +11,34 @@ const apiLink = 'http://pollitic.herokuapp.com/api/';
 class PollsFull extends Component {
     state = {
         status: 'pending',
+        polls: [],
+        totalPages: null,
+        page: 1,
+        perPage: 10,
+        scrolling: false,
     };
 
     pendingPromises = [];
 
+    componentWillMount() {
+        window.addEventListener('scroll', e => {
+            this.handleScroll(e);
+        });
+    }
+
     componentDidMount() {
-        this.getApiData(this.props.sort, this.props.context);
+        this.getApiData(this.props.sort, this.props.context, true);
     }
 
     componentDidUpdate(prevProps) {
-        if (
-            this.props.sort !== prevProps.sort ||
-            this.props.context !== prevProps.context
-        ) {
-            this.getApiData(this.props.sort, this.props.context);
+        if (this.props.sort !== prevProps.sort || this.props.context !== prevProps.context) {
+            this.getApiData(this.props.sort, this.props.context, true);
         }
     }
 
     componentWillUnmount() {
         this.pendingPromises.map(p => p.cancel());
+        window.removeEventListener('scroll', this.handleScroll);
     }
 
     appendPendingPromise = promise => {
@@ -40,15 +49,21 @@ class PollsFull extends Component {
         this.pendingPromises = this.pendingPromises.filter(p => p !== promise);
     };
 
-    getApiData = (sortBy, contextBy) => {
-        this.setState({
-            status: 'pending',
-        });
+    getApiData = (sortBy, contextBy, initLoad) => {
+        initLoad = initLoad ? true : false;
+
+        if (initLoad) {
+            this.setState({
+                status: 'pending',
+            });
+        }
 
         const wrappedPromise = cancelablePromise(
             axios.get(apiLink + contextBy, {
                 params: {
                     sort: sortBy,
+                    page: this.state.page,
+                    perPage: this.state.perPage,
                 },
             })
         );
@@ -58,10 +73,23 @@ class PollsFull extends Component {
         return wrappedPromise.promise
             .then(response => {
                 if (response.data.status === 'success') {
-                    this.setState({
-                        apiData: response.data,
-                        status: response.statusText,
-                    });
+                    if (initLoad) {
+                        this.setState({
+                            polls: [...response.data.data.polls],
+                            status: response.statusText,
+                            page: response.data.data.page,
+                            totalPages: response.data.data.totalPages,
+                            scrolling: false,
+                        });
+                    } else {
+                        this.setState({
+                            polls: [...this.state.polls, ...response.data.data.polls],
+                            status: response.statusText,
+                            page: response.data.data.page,
+                            totalPages: response.data.data.totalPages,
+                            scrolling: false,
+                        });
+                    }
                 }
             })
             .then(() => this.removePendingPromise(wrappedPromise))
@@ -75,6 +103,28 @@ class PollsFull extends Component {
             });
     };
 
+    handleScroll = e => {
+        const { scrolling, totalPages, page } = this.state;
+        if (scrolling) return;
+        if (totalPages <= page) return;
+        const lastPoll = document.querySelector('.pollitic-item.padded-white > .row > .col:last-child');
+        const lastPollOffset = lastPoll.offsetTop + lastPoll.clientHeight;
+        const pageOffset = window.pageYOffset + window.innerHeight;
+        const bottomOffset = 20;
+        if (pageOffset > lastPollOffset - bottomOffset) this.loadMorePolls();
+    };
+
+    loadMorePolls = () => {
+        this.setState(
+            prevState => ({
+                page: ++prevState.page,
+                scrolling: true,
+                status: 'scrolling',
+            }),
+            () => this.getApiData(this.props.sort, this.props.context)
+        );
+    };
+
     getHeaderName = (sortBy, contextBy) => {
         if (contextBy === 'closed') return 'დასრულებული';
         if (sortBy === 'hot') return 'კონტროვერსიალური';
@@ -82,21 +132,22 @@ class PollsFull extends Component {
     };
 
     render() {
+        const pollsDisplay = (
+            <React.Fragment>
+                <h3>{this.getHeaderName(this.props.sort, this.props.context)}</h3>
+                <div className="row">
+                    <PollDisplay polls={this.state.polls} size="large" />
+                </div>
+            </React.Fragment>
+        );
+
         if (this.state.status === 'OK') {
+            return pollsDisplay;
+        } else if (this.state.status === 'scrolling') {
             return (
                 <React.Fragment>
-                    <h3>
-                        {this.getHeaderName(
-                            this.props.sort,
-                            this.props.context
-                        )}
-                    </h3>
-                    <div className="row">
-                        <PollDisplay
-                            polls={this.state.apiData.data.polls}
-                            size="large"
-                        />
-                    </div>
+                    {pollsDisplay}
+                    <PreLoader />
                 </React.Fragment>
             );
         } else if (this.state.status === 'Not Found') {
--- src/ParentComponents/Polls.js
+++ src/ParentComponents/Polls.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import PollsFull from '../ChildComponents/PollsFull';
-import PollsMin from '../ChildComponents/PollsMin';
 
 const Polls = ({ location }) => {
     const params = new URLSearchParams(location.search);
     const sortBy = params.get('sort');
     const contextBy = params.get('context');
 
-    const pollMinSortBy = {
-        new: 'hot',
-        hot: 'new',
-    };
-
     return (
         <React.Fragment>
             <PollsFull sort={sortBy} context={contextBy} />
-            <PollsMin sort={pollMinSortBy[sortBy]} />
         </React.Fragment>
     );
 };
